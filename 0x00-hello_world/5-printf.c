6968ec3dc1b2e2f210d2c7535ac8fc31a4bb0cc7
elmahdiouyaaz <elmahdiouyaaz@gmail.com>
2023-03-16 07:55:23 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- * main - print the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - use printf
+ * Return: 0
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-

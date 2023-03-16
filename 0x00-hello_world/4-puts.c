8c6712a30588fd18c08a44dedb61748bdd3fe6ec
elmahdiouyaaz <elmahdiouyaaz@gmail.com>
2023-03-16 07:16:59 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/* 
+ * The main function is the entry point of the program.
+ * It prints a string to the standard output using the 
+ * puts function and returns 0 to indicate successful 
+ * termination of the program.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

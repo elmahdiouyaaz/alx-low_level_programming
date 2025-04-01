b734655891bfb0184cff886aa83e988700f2cda4
elmahdiouyaaz <the3nginner@proton.me>
2025-04-01 04:11:27 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *
+ * Description: printing a line using puts.
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\0");
